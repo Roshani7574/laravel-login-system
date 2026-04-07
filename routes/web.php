252a23d077147cc9f7d076e5507550a36a47060e
Roshani Shingate <roshanishingate12@gmail.com>
2026-04-08 01:30:19 +0530

    Fix merge conflict in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Admin\AuthController as AdminAuthController;
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\ProductController;
-
-
-/* User Routes */
-
-Route::get('/', function () {
-    return redirect('/register');
-});
-
-Route::get('/register', [AuthController::class,'showRegister'])->name('register');
-Route::post('/register', [AuthController::class,'register']);
-
-Route::get('/login', [AuthController::class,'showLogin'])->name('login');
-Route::post('/login', [AuthController::class,'login']);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth')->name('user.dashboard');
-
-
-/* Admin Routes */
-
-Route::prefix('admin')->group(function () {
-
-    Route::get('/login', [AdminAuthController::class,'showLoginForm'])->name('admin.login');
-    Route::post('/login', [AdminAuthController::class,'login']);
-
-    Route::middleware('admin')->group(function () {
-
-        Route::get('/', [AdminAuthController::class,'adminDashboard'])->name('admin.dashboard');
-
-        Route::resource('categories', CategoryController::class);
-        Route::resource('products', ProductController::class);
-
-        Route::get('categories/{id}/subcategories', [CategoryController::class,'getSubcategories']);
-
-    });
-
-    Route::get('/logout', [AdminAuthController::class,'logout'])->name('admin.logout');
-
-});
-=======
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\Admin\ProductController;
@@ -97,4 +51,3 @@ Route::get('/success', function () {
 
 
 
->>>>>>> d024ab1 (Update project: Like a E-commerce website)
